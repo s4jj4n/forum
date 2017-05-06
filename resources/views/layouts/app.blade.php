22a11b6c695cddac33c99df971a1a14fda5e1b23
sajjan <s4jj4n@gmail.com>
2017-05-06 10:44:20 +0545

    13. Extracting to a View Composers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                 Channels <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <li><a href="{{ route('threads.index', [$channel->slug]) }}">{{ $channel->name }}</a></li>
                                 @endforeach
                             </ul>
