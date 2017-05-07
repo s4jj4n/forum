418caffe28faf7206d0bb55acc11d464ac1ec7f5
sajjan <s4jj4n@gmail.com>
2017-05-07 19:47:48 +0545

    21. Global scopes and further query reduction

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     protected static function boot()
     {
         parent::boot();
@@ -15,6 +17,9 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function ($builder) {
             $builder->withCount('replies');
         });
+//        static::addGlobalScope('creator', function ($builder) {
+//            $builder->with('creator');
+//        });
     }
 
     public function path()
@@ -25,9 +30,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
