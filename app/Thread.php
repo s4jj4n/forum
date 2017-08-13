8eaa53bd1ce2003dc8b77b8bba72b196d4142744
Sajjan <s4jj4n@gmail.com>
2017-08-13 17:53:36 +0545

    25. How to construct an activity feeds

--- app/Thread.php
+++ app/Thread.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    use RecordsActivity;
+
     protected $guarded = [];
 
     protected $with = ['creator', 'channel'];
@@ -24,6 +26,8 @@ class Thread extends Model
         static::deleting(function ($thread) {
             $thread->replies()->delete();
         });
+
+
     }
 
     public function path()
