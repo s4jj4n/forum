6ea1fe60f603bc6a7dd15d6ceca853bc15f44195
Sajjan <s4jj4n@gmail.com>
2017-09-17 19:08:50 +0545

    26. How to construct an activity feeds Part 2

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -7,35 +7,17 @@
                 <div class="page-header">
                     <h1>
                         {{ $profileUser->name }}
-                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                     </h1>
                 </div>
 
-                @foreach ($threads as $thread)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                        </span>
-                                <span>
-                            {{ $thread->created_at->diffForHumans() }}
-                        </span>
-                            </div>
-                        </div>
+                @foreach ($activities as $date => $activity)
+                    <h3 class="page-header">{{ $date }}</h3>
 
-                        <div class="panel-body">
-                            <div class="body">{{ $thread->body }}</div>
-                        </div>
-                    </div>
+                    @foreach ($activity as $record)
+                        @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
                 @endforeach
-                {{ $threads->links() }}
             </div>
         </div>
-
-
-
     </div>
-
 @endsection
